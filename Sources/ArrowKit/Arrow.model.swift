0d5526accf5678344c76a133640391d2e74465e7
Valentin Knabel <dev@vknabel.com>
2018-01-07 14:13:21 +0100

    Allow arrows not to declare predefined values

--- Sources/ArrowKit/Arrow.model.swift
+++ Sources/ArrowKit/Arrow.model.swift
@@ -1,6 +1,3 @@
 public protocol Arrow: Decodable {
-  var arrow: String { get }
-  var help: String? { get }
-
-func fire(archerfile: Archerfile, arguments: [String]) throws
+  func fire(archerfile: Archerfile, arguments: [String]) throws
 }
